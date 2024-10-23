93374e0377d61dfd4ed92d720abc4aab5c75f471
Shravya Panumati <151785937+ShravyaPanumati@users.noreply.github.com>
2024-10-23 13:07:02 -0400

    Create config.py
    Managing database credentials

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# SQL Server connection details
+DB_CONFIG = {
+    'server': ' ',  # Replace with your instance public IP
+    'database': 'myappdb',     # Replace with your database name
+    'username': 'myuser',       # Replace with your username
+    'password': '  ',   # Replace with your password
+    'driver': '{ODBC Driver 17 for SQL Server}'  # ODBC driver
+}
